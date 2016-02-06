4baeac1704ba3c7a633150637e85e0a86e44f332
koojunho <koojh@9cells.com>
2016-02-06 21:45:39 +0900

    구현 변경

--- Console/ToggleComposerCommand.php
+++ Console/ToggleComposerCommand.php
@@ -1,7 +1,8 @@
 <?php
 
-use NineCells\Dev\ToggleComposer\Console;
+namespace NineCells\Dev\ToggleComposer\Console;
 
+use File;
 use Illuminate\Console\Command;
 
 class ToggleComposerCommand extends Command
@@ -12,6 +13,18 @@ class ToggleComposerCommand extends Command
 
     public function fire()
     {
-        echo File::get('composer.json');
+        $composer_json = File::get('composer.json');
+        $json_obj = json_decode($composer_json, true);
+
+        $dev_mode = isset($json_obj['repositories']);
+
+        if ($dev_mode) {
+            unset($json_obj['repositories']);
+            File::put('composer.json', json_encode($json_obj, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+            $this->info('이제 배포 모드 입니다.');
+        } else {
+            File::put('composer.json', File::get('composer_dev.json'));
+            $this->info('이제 개발 모드 입니다.');
+        }
     }
 }
